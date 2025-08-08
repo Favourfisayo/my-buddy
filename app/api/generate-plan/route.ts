ecf028ae9080b72e86fe459bc1e400c0778a0706
Favourfisayo <Favourfisayo5@gmail.com>
2025-08-08 15:24:33 +0100

    Removed auth's api route for google's provider

--- app/api/generate-plan/route.ts
+++
@@ -1,8 +0,0 @@
-import { generateLLMOutput } from "@/features/llm/getLLMOutput"
-import { NextResponse } from "next/server"
-export async function POST(req: Request) {
-    const input = await req.json()
-    const plan = await generateLLMOutput(input)
-    return NextResponse.json(plan)
-  }
-  
